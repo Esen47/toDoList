c3613e1eb1490562b0b3018f8f07a6ccff920598
Sasa Nikolic <sasa.nikolic225@gmail.com>
2019-11-02 12:12:09 -0400

    added functionality for searching and deleting the items

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
+//html template used to add the item to the list once user submits
 const generateTemplate = todo => {
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -11,21 +13,45 @@ const generateTemplate = todo => {
     list.innerHTML += html;
 }
 
+//getting user input
 addForm.addEventListener('submit', e => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
     if(todo.length){
         generateTemplate(todo);
         addForm.reset();
+    //popup window to request input to be added
     } else {
          (() => {
             const el = document.createElement('div');
             el.classList.add('warning');
-            el.innerHTML = 'Please enter a desired action';
+            el.innerHTML = '<p>Please enter a desired action</p>';
             document.body.appendChild(el);
             setTimeout(() => {
                 document.body.removeChild(el);
             },2500)
         })();
     }
+});
+
+//deleting items
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+//filtering the matching and non matching items in the search
+const filterItems = (item) => {
+    Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(item))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(item))
+    .forEach((todo) => todo.classList.remove('filtered'));
+};
+//searching items in the list
+search.addEventListener('keyup', () => {
+    const item = search.value.trim().toLowerCase();
+    filterItems(item);
 });
